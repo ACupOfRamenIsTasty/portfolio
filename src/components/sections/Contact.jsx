f55b3194f6908214b1cfce7635ec0420eab41a58
ACupOfRamenIsTasty <daehyeon5583@gmail.com>
2025-08-06 00:15:45 -0400

    Fixed .env for contact form

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -25,7 +25,7 @@ export const Contact = () => {
             className="min-h-screen flex items-center justify-center py-20"
         >
             <RevealOnScroll>
-                <div className="px-4 w-150">
+                <div className="px-4 w-full md:w-150">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-orange-700 to-yellow-400 bg-clip-text text-transparent text-center">
                         Contact Me
                     </h2>
