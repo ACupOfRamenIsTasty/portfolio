8998d23f600204661e62bf81c44239bf87978143
ACupOfRamenIsTasty <daehyeon5583@gmail.com>
2026-05-27 20:48:53 -0400

    Updated multi node project description

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -103,7 +103,7 @@ export const Projects = () => {
                                 01/2026 - 04/2026
                             </h4>
                             <p className="text-gray-400 mb-4">
-                                Assembled and networked a 6 fire panels to support UL/ULC testing of FX-MNS-6000 devices
+                                Assembled and networked 6 FX-MNS-6000 unitsto support UL/ULC testing
                             </p>
                             <div className="flex flex-wrap gap-2 mb-4">
                                 {["PLCs", "Circuit Design", "Power Systems", "Cygwin", "Tera Term"].map((skill, key) => (
